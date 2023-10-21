801951a8acbf52161eaca20afee3e0fff8f3b79d
practise <asmita21jagadale@gmail.com>
2023-10-21 23:25:48 +0530

    update

--- demo.py
+++ demo.py
@@ -26,8 +26,8 @@ def display(self):
     print(f'Subject marks:{self.subject} - {self.marks1}')
 
 stu1 = Result()
-stu1.get_data(1101,"Asmita jagadale")
+stu1.get_data(1101,"Asmita Jagadale")
 stu1.set_name("Tennis", "40/50")
-stu1.set_name1("Advanced Python", "48/50")
+stu1.set_name1("Advanced Python", "45/50")
 stu1.set_data("MIT ADT University","MITU19IMBI0012")
 stu1.display()
